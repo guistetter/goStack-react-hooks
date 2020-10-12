1cf6ddf5e517c01f66c544742b2fddfbcfd8fcce
guistetter <guilherme_stetter@hotmail.com>
2020-10-12 15:13:14 -0300

    add react-hooks rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,25 +1,26 @@
 module.exports = {
-    env: {
-      browser: true,
-      es6: true,
+  env: {
+    browser: true,
+    es6: true,
+  },
+  extends: ['plugin:react/recommended', 'airbnb', 'prettier', 'prettier/react'],
+  parser: 'babel-eslint',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
     },
-    extends: ['plugin:react/recommended', 'airbnb', 'prettier', 'prettier/react'],
-    parser: 'babel-eslint',
-    parserOptions: {
-      ecmaFeatures: {
-        jsx: true,
-      },
-      ecmaVersion: 12,
-      sourceType: 'module',
-    },
-    plugins: ['react', 'prettier'],
-    rules: {
-      'react/state-in-constructor': 'off',
-      'prettier/prettier': 'error',
-      'react/jsx-filename-extension': ['warn', { extensions: ['.jsx', '.js'] }],
-      'import/prefer-default-export': 'off',
-      'no-console':["error", {allow:["tron"]}],
-      'no-param-reassign': 'off'
-    },
-  };
-  
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: ['react', 'prettier', 'react-hooks'],
+  rules: {
+    'react/state-in-constructor': 'off',
+    'prettier/prettier': 'error',
+    'react/jsx-filename-extension': ['warn', { extensions: ['.jsx', '.js'] }],
+    'import/prefer-default-export': 'off',
+    'no-console': ['error', { allow: ['tron'] }],
+    'no-param-reassign': 'off',
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
+  },
+};
