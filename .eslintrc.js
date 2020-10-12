6f947fc3e280f10c4738b8a055a07065921c08aa
guistetter <guilherme_stetter@hotmail.com>
2020-10-12 14:48:47 -0300

    create project with eslint configs

---
+++ .eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+    env: {
+      browser: true,
+      es6: true,
+    },
+    extends: ['plugin:react/recommended', 'airbnb', 'prettier', 'prettier/react'],
+    parser: 'babel-eslint',
+    parserOptions: {
+      ecmaFeatures: {
+        jsx: true,
+      },
+      ecmaVersion: 12,
+      sourceType: 'module',
+    },
+    plugins: ['react', 'prettier'],
+    rules: {
+      'react/state-in-constructor': 'off',
+      'prettier/prettier': 'error',
+      'react/jsx-filename-extension': ['warn', { extensions: ['.jsx', '.js'] }],
+      'import/prefer-default-export': 'off',
+      'no-console':["error", {allow:["tron"]}],
+      'no-param-reassign': 'off'
+    },
+  };
+  
